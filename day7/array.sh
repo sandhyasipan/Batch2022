cd785b91f906d76281b62b76780960fab20790a1
sandhyasipan <sandhyaswain70@gmail.com>
2022-04-19 17:51:34 +0530

    added day7 programs

---
+++ day7/array.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+declare -a fruit
+counter=0
+fruits[((counter++))]="Apple"
+fruits[((counter++))]="Banana"
+fruits[((counter++))]="100"
+
+
+echo ${fruits[@]}
+
+echo ${fruits[1]}
+
+echo ${!fruits[@]}
+
+echo ${#fruits[@]}
+
+for index in ${!fruits[@]}
+do
+    echo $index=${fruits[$index]}
+done
